3b39b400e3a9311496b3ef975cb62f2b7771ccce
RKstigo <rkstigo@gmail.com>
2025-11-05 22:33:41 -0300

    Update fetchJupPrice.js

--- fetchJupPrice.js
+++ fetchJupPrice.js
@@ -3,7 +3,16 @@ import { loadGistFile, updateGistFiles, updateHourlyGist } from "./gist.js";
 
 // Token mint -> CSV file mapping
 const TOKENS = {
-  "JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN": "jupPrices.csv"
+  "JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN": "jupPrices.csv",
+  "pumpCmXqMfrsAkQ5r49WcJnRayYRqmXz6ae8H7H9Dfn": "pumpPrices.csv",
+  "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263": "bonkPrices.csv",
+  "2zMMhcVQEXDtdE6vsFS7S7D5oUodfJHE8vd1gnBouauv": "penguPrices.csv",
+  "4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R": "rayPrices.csv",
+  "EKpQGSJtjMFqKZ9KQanSqYXRcF8fBopzLHYxdM65zcjm": "wifPrices.csv",
+  "7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr": "popcatPrices.csv",
+  "J3NKxxXZcnNiMjKw9hYb2K4LUxgwB6t1FtPtQVsv3KFr": "spxPrices.csv",
+  "9BB6NFEcjBCtnNLFko2FqVQBq8HHM13kCyYcdQbgpump": "fartPrices.csv",
+  "27G8MtK7VtTcCHkpASjSDdkWWYfoqT6ggEuKidVJidD4": "jlpPrices.csv"
 };
 
 const API_URL = `https://lite-api.jup.ag/price/v3?ids=${Object.keys(TOKENS).join(",")}`;
